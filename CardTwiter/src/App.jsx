6a25d0b1828df2f748b06680ae9fd5578880c3d0
cshica <bleglaiger@gmail.com>
2023-12-24 13:25:03 -0600

    Inicio

---
+++ CardTwiter/src/App.jsx
@@ -0,0 +1,14 @@
+export function App()
+{
+    return (
+        <article>
+            <header>
+                <img alt="El Programador" src="https://pbs.twimg.com/profile_images/1586373389563973632/0bqilLWJ_400x400.jpg"/>
+                <div>
+                    <strong>Acss</strong>
+                    <strong>@bleglaiger</strong>
+                </div>
+            </header>
+        </article>
+    )
+}
